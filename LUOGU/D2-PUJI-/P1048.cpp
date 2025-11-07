d56da0d1b31ac3b51573efedc36da9da2b8d1aad
JackPeng-Coder <jackpeng-coder@qq.com>
2025-11-07 18:14:51 +0800

    feat: 添加动态规划解题代码
    添加三道动态规划问题的解题代码：
    1. P1049.cpp 解决装箱问题
    2. P1048.cpp 解决采药问题
    3. P1616.cpp 解决疯狂的采药问题
    
    所有代码均使用标准C++实现，包含必要的输入输出处理

---
+++ LUOGU/D2-PUJI-/P1048.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int T, M;
+    cin >> T >> M;
+    vector<pair<int, int>> vec(M);
+    vector<int> dp(T + 1);
+    for (int i = 0; i < M; i++) {
+        cin >> vec[i].first >> vec[i].second;
+    }
+    for (int i = 0; i < M; i++) {
+        for (int j = T; j >= vec[i].first; j--) {
+            dp[j] = max(dp[j], dp[j - vec[i].first] + vec[i].second);
+        }
+    }
+    cout << dp[T] << endl;
+}
